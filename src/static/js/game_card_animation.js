03a0cc21f254c1728c6e178fc8ef82df32307ca2
Jesús Muñoz Martín-Caro <jesmun04@ucm.es>
2025-11-07 02:21:12 +0100

    Mejoras varias en la apariencia de la página web
    Incluye diversas mejoras menores en la disposición de los elementos, las animaciones, los estilos, etc. de varias páginas.

--- src/static/js/game_card_animation.js
+++ src/static/js/game_card_animation.js
@@ -1,14 +1,22 @@
-// Efectos interactivos para el dashboard
+// Efectos de giro y traslación hacia arriba
 document.addEventListener('DOMContentLoaded', function() {
-    // Efecto de escaneo para las cards
-    const cards = document.querySelectorAll('.animated-card');
+    const elems = document.querySelectorAll('.animated-rotate, .animated-translateY');
     
-    cards.forEach((card, index) => {
-        setTimeout(() => {
-            card.style.transition = 'all 0.6s cubic-bezier(0.25, 0.46, 0.45, 0.94)';
-            card.style.opacity = '1';
-            card.style.scale = '1'; // Requiere CSS Transforms Module Level 2
-            card.style.rotate = 'Y 0deg'; // Requiere CSS Transforms Module Level 2
-        }, index * 150);
+    elems.forEach((elem, index) => {
+        if (elem.classList.contains('animated-rotate')) {
+            setTimeout(() => {
+                elem.style.transition = 'all 0.6s cubic-bezier(0.25, 0.46, 0.45, 0.94)';
+                elem.style.opacity = '1';
+                elem.style.scale = '1'; // Requiere CSS Transforms Module Level 2
+                elem.style.rotate = 'Y 0deg'; // Requiere CSS Transforms Module Level 2
+            }, index * 150);
+        }
+        else if (elem.classList.contains('animated-translateY')) {
+            setTimeout(() => {
+                elem.style.transition = 'all 0.6s ease';
+                elem.style.opacity = '1';
+                elem.style.translate = '0 0'; // Requiere CSS Transforms Module Level 2
+            }, index * 150);
+        }
     });
 });
